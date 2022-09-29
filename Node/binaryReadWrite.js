39fc29dc00486d063c86a004e252ded8a1193812
AllenLee\FuckGitConfigs <648384410li@gmail.com>
2022-09-29 13:40:04 +0800

    readPerPage

--- Node/binaryReadWrite.js
+++ Node/binaryReadWrite.js
@@ -1,6 +1,12 @@
 const fs = require("fs");
 const path = require("path");
-const { constants } = require("buffer");
+const fsRead = (...args) =>
+  new Promise((resolve, reject) =>
+    fs.read(...args, (err, bytesRead, buffer) => {
+      if (err) reject(err);
+      else resolve(bytesRead, buffer);
+    })
+  );
 
 async function open(fileName, openFlags, cacheSize, pageSize) {
   cacheSize = cacheSize || 4096 * 64;
@@ -24,25 +30,59 @@ class BinaryFile {
    */
   constructor(fd, stats, pageSize) {
     this.fd = fd;
-    this.pageSize = pageSize || 1 << 8;
+    this.pageSize = pageSize || 0x1 << 8;
     while (this.pageSize < stats.blksize) {
-      this.pageSize *= 2;
+      this.pageSize = this.pageSize << 1;
     }
     this.totalSize = stats.size;
+    this.readWatermark = 1024 ** 2; // 1mb
+    this.readLimit = 1024 ** 4; // 1g
   }
 
   /**
    * TODO:
    * 1. write
-   * 2. *perPage
-   * 3. error handler
+   * 2. error handler
    */
 
   /**
-   * TODO: Use read for smaller size rather than stream. Handle other events such as `close`. Use the stream property instead of requesting a stream for each read
+   * TODO: Handle errors
+   */
+  async *readPerPage(pos, len, pageSize) {
+    if (pos > this.totalSize || len <= 0 || pageSize <= 0)
+      return Buffer.alloc(0);
+
+    const totalPages = Math.ceil(
+      Math.min(len, this.totalSize - pos) / pageSize
+    );
+
+    let curPage = 0,
+      curBuffer = Buffer.alloc(pageSize);
+
+    while (curPage < totalPages) {
+      const { bytesRead } = await fsRead(
+        this.fd.fd,
+        curBuffer,
+        0,
+        pageSize,
+        curPage * pageSize + pos
+      );
+      if (bytesRead <= 0) {
+        break;
+      }
+
+      yield curBuffer;
+      curPage++;
+    }
+
+    return curBuffer;
+  }
+
+  /**
+   * TODO: Handle other events such as `close`. Use the stream property instead of requesting a stream for each read
    */
   async *readPerBlock(pos, len) {
-    if (pos > this.totalSize) return Buffer.alloc(0);
+    if (pos > this.totalSize || len <= 0) return Buffer.alloc(0);
 
     const end = len + pos > this.totalSize - pos ? undefined : len + pos;
     const stream = this.fd.createReadStream({
@@ -59,12 +99,16 @@ class BinaryFile {
       await new Promise((resolve) => {
         stream.on("data", (chunk) => {
           curBuffer = typeof chunk === "string" ? Buffer.from(chunk) : chunk;
-          resolve();
-
           restLen -= curBuffer.length;
           if (restLen <= 0) hasData = false;
+
+          resolve();
         });
         stream.on("end", () => {
+          hasData = false;
+          resolve();
+        });
+        stream.on("close", () => {
           hasData = false;
           resolve();
         });
@@ -78,14 +122,20 @@ class BinaryFile {
   }
 
   /**
-   * TODO: Handle errors. Handle more len than 1gig
+   * TODO: Handle errors.
    * @param {number} pos
    * @param {number} len bytes length
-   * @returns {Promise<Buffer | Buffer[]>} return `Buffer[]` if `len > buffers.constants.MAX_LENGTH` else `Buffer`
+   * @returns {Promise<Buffer | Buffer[]>} return `Buffer[]` if `len > pageSize` else `Buffer`
    */
   async read(pos, len) {
+    if (len > this.readLimit)
+      throw `You should not use 'read' to read more than 1g of data in one go. It's recommended to handle 'perBlock' or 'perPage' by yourself.`;
+
+    const waterOverflow = len > this.readWatermark;
     const buffers = [];
-    const iter = this.readPerBlock(pos, len);
+    const iter = waterOverflow
+      ? this.readPerBlock(pos, len)
+      : this.readPerPage(pos, len, Math.min(this.pageSize, len));
     let data = await iter.next();
     while (!data.done) {
       if (data.value) buffers.push(data.value);
@@ -108,7 +158,7 @@ class BinaryFile {
       );
     }
 
-    if (len > constants.MAX_LENGTH) {
+    if (len > this.pageSize) {
       return buffers;
     }
     return Buffer.concat(buffers);
